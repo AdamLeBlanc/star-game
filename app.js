3499114a420a96fda167f034c3c072e040dec594
Adam LeBlanc <leblanca37@gmail.com>
2018-02-21 13:19:33 -0400

    Got the game into the DOM

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.error("YOU NEED TO MAKE THE GAME!");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {
+  preload,
+  create,
+  update
+});
+
+function preload() {}
+
+function create() {}
+
+function update() {}
