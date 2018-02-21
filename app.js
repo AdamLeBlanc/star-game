f39db1dabcd8470a5a5f048a6193bf81c6d620e3
Adam LeBlanc <leblanca37@gmail.com>
2018-02-21 13:38:49 -0400

    Added the platforms for the player to jump on

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ function preload() {
 }
 
 function create() {
+  var ground;
+  var ledge;
+  // turn on the physics system
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
   game.add.sprite(0, 0, "sky");
+
+  platforms = game.add.group();
+  platforms.enableBody = true; // enable physics body by default on all sprites
+  ground = platforms.create(0, game.world.height - 64, "ground");
+  ground.scale.setTo(2, 2); // double the size
+  ground.body.immovable = true; // make it so it doesn't move on collisions
+
+  ledge = platforms.create(400, 400, "ground");
+  ledge.body.immovable = true;
+
+  // just reuse the same variable for the next ledge
+  ledge = platforms.create(-150, 250, "ground");
+  ledge.body.immovable = true;
 }
 
 function update() {}
