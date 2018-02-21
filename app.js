bf6d6859bd0351edde185c45c3738ed7a3d87ecf
Adam LeBlanc <leblanca37@gmail.com>
2018-02-21 13:26:54 -0400

    loaded assets and added some more style to the html to make it easier to see the game

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {
   update
 });
 
-function preload() {}
+function preload() {
+  game.load.image("sky", "assets/sky.png");
+  game.load.image("ground", "assets/platform.png");
+  game.load.image("star", "assets/star.png");
+  // each sprite is 32x48
+  game.load.spritesheet("dude", "assets/dude.png", 32, 48);
+}
 
-function create() {}
+function create() {
+  game.add.sprite(0, 0, "sky");
+}
 
 function update() {}
