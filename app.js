628703b05c65da116f808189d8bd3d2306af8b53
Adam LeBlanc <leblanca37@gmail.com>
2018-02-21 14:07:52 -0400

    added stars:

--- app.js
+++ app.js
@@ -46,10 +46,22 @@ function create() {
   player.animations.add("right", [5, 6, 7, 8], 10, true);
 
   cursors = game.input.keyboard.createCursorKeys(); // manages keyboard input for us
+
+  stars = game.add.group();
+  stars.enableBody = true; // enable body by default on all
+
+  // spread starts out 70px apart
+  for (var i = 0; i < 12; i++) {
+    var star = stars.create(i * 70, 0, "star");
+    star.body.gravity.y = 100;
+    star.body.bounce.y = 0.7 + Math.random() * 0.2; // random bounce
+  }
 }
 
 function update() {
   var hitPlatform = game.physics.arcade.collide(player, platforms); // check collisions
+  game.physics.arcade.collide(stars, platforms); // collide with platforms
+  game.physics.arcade.overlap(player, stars, collectStar, null, this); // collect the stars when the player runs into them
   player.body.velocity.x = 0; // reset player movement
   if (cursors.left.isDown) {
     player.body.velocity.x = -150;
@@ -65,4 +77,8 @@ function update() {
   if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
     player.body.velocity.y = -350;
   }
+}
+
+function collectStar(player, star) {
+  star.kill();
 }
